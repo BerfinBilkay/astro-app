e0daaaeea957739a2fd915462ad9048417a2471c
Berfin <112422548+BerfinBilkay@users.noreply.github.com>
2025-05-17 01:17:34 +0300

    Initialize

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,7 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyDH4_YvFSCFV0QkVkyFhmfLyuIJC1jUZf8",
-    authDomain: "ai-astrology-c6680.firebaseapp.com",
-    projectId: "ai-astrology-c6680",
-    storageBucket: "ai-astrology-c6680.firebasestorage.app",
-    messagingSenderId: "1092610452713",
-    appId: "1:1092610452713:web:7c859d5f667bf2a4aed478"
+
   },
   openai: {
     apiKey: '',
